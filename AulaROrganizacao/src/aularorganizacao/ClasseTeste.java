4b5791b62c160f372debcc53d75fdf7b3d1e5c68
Maximagraca <31247388+Maximagraca@users.noreply.github.com>
2017-09-05 14:12:50 +0200

    Add files via upload
    ClasseTesteCorrecao

---
+++ AulaROrganizacao/src/aularorganizacao/ClasseTeste.java
@@ -0,0 +1,33 @@
+
+package aularorganizacao;
+
+/**
+ *
+ * @author Alfredo
+ * @author maxima
+ */
+public class ClasseTeste {
+    private String manuais;
+
+    public ClasseTeste(String manuais) {
+        this.manuais = manuais;
+        
+        
+        
+    }
+
+    public String getManuais() {
+        return manuais;
+    }
+
+    public void setManuais(String manuais) {
+        this.manuais = manuais;
+    }
+    
+    
+    
+    
+    
+ 
+        
+    }
